e1344e7f7af618deb1400d1e6b02abb5c1adc535
atheelarouge <atheelarouge@gmail.com>
2021-02-06 18:51:45 +0300

    seventh com

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-let countDownDate = new Date("Feb 20, 2021 09:00:00").getTime();
+let countDownDate = new Date("Feb 27, 2021 09:00:00").getTime();
 let finish = document.querySelector('.finish');
 
 //Update countdown every one second
@@ -10,6 +10,7 @@ let x = setInterval(function() {
 
     // Find the distance between now and countdown date
     let distance = countDownDate - now;
+    console.log(distance)
 
     // Time calculation for days, hours, minutes and seconds
     let days = Math.floor(distance / (1000*60*60*24));
@@ -30,23 +31,23 @@ let x = setInterval(function() {
     if(distance < 0) {
         clearInterval(x)
         finish.innerText = "Let's Start NodeJs & React";
-    } else if (distance < 1175503119) {
+    } else if (distance < 1779146250) {
         finish.innerText = "Countdown Start!";
-    } else if (distance < 1057952807){
+    } else if (distance < 1579146250){
         finish.innerText = "Show me what you can do..."
-    }else if (distance < 822852183){
+    }else if (distance < 1379146250){
         finish.innerText = "Don't be relaxed :)"
-    } else if(distance < 783668746) {
+    } else if(distance < 1186097500) {
         finish.innerText = "1/3 of Time Expired";
-    } else if (distance < 587751559) {
+    } else if (distance < 889573125) {
         finish.innerText = 'You Half Your Time';
-    } else if (distance < 391834373) {
+    } else if (distance < 593048750) {
         finish.innerText = "You have only 1/3 Time";
-    } else if (distance < 293875779) {
-        finish.innerText
-    } else if (distance < 293875779) {
+    } else if (distance < 493048750) {
+        finish.innerText = "Time time time..."
+    } else if (distance < 393875779) {
         finish.innerText = "Your time is tithen..."
-    } else if (distance < 117550311) {
+    } else if (distance < 217550311) {
         finish.innerText = "Your time nearly up..."
     } else if (distance < 77550311) {
         finish.innerText = "Hurry up!"
